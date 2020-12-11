915408c95adf9dfd5ece08dba5751d145428acf1
Rey Mendez <rmendez15@mail.csuchico.edu>
2020-12-11 04:22:12 +0000

    class added

--- main.cpp
+++ main.cpp
@@ -1,60 +1,41 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include "libraries/bitmap.h"
+#include <string>
+#include "user.h"
 using namespace std;
 
 int main()
 {
-  Bitmap image;
-  vector <vector <Pixel> > bmp;
-  Pixel rgb;
-
-  //read a file example.bmp and convert it to a pixel matrix
-  image.open("bmp_24.bmp");
-
-  //verify that the file opened was a valid image
-  bool validBmp = image.isImage();
-
-  if( validBmp == true )
-  {
-    bmp = image.toPixelMatrix();
-  
-
-    //take all the redness out of the top-left pixel
-    rgb = bmp[0][0];
-    rgb.red = 0; 
-
-    //put changed image back into matrix, update the bitmap and save it
-    bmp[0][0] = rgb;
-    image.fromPixelMatrix(bmp);
-    image.save("example.bmp");
-  }
-  /*
   vector<string>answerVector;
+  user test;
 
-  string answer, decision, firstName, lastName;
+  string answer, decision, firstName, lastName, imageColor1, imageColor2, imageColor3;
   int age;
   bool glasses;
-  char decision2; 
+  char decision2;
+
 
   ofstream outfile;
   outfile.open("results.txt");
 
   cout << "Hi, what is your first name?:";
   cin >> firstName;
+  test.setFirstName(firstName);
 
-  outfile << "First Name: " << firstName << endl;
+  outfile << "First Name: " << test.getFirstName()<< endl;
 
   cout << "What is your last name?:";
   cin >> lastName;
+  test.setLastName(lastName);
 
-  outfile << "Last Name: " << lastName << endl;
+  outfile << "Last Name: " << test.getLastName() << endl;
 
   cout << "How old are you?:";
   cin >> age;
+  test.setAge(age);
 
-  outfile << "Age: " << age << endl;
+  outfile << "Age: " << test.getAge() << endl;
 
   cout << "Do you wear glasses? (Y/N):";
   cin >> decision2;
@@ -67,33 +48,63 @@ int main()
   {
     glasses = false;  
   }
-  
-  outfile << "Use of glasses(0=No,1=Yes): " <<glasses << endl;
+  test.setGlasses(glasses);
 
+  outfile << "Use of glasses(0=No,1=Yes): " <<test.getGlasses() << endl;
+
+  cout << "Please open file userimage.bmp. Do you see the color red?(Yes/No)" << endl;
+  cin >> imageColor1;
+
+  if (imageColor1 == "Yes"){
+    cout << "Correct" << endl;
+  }
+  else if (imageColor1 == "No") {
+    cout << "Incorrect. May need to turn on colorblind settings." << endl;
+  }
+
+  cout << "Do you see the color green?(Yes/No)" << endl;
+  cin >> imageColor2;
+
+  if (imageColor2 == "Yes"){
+    cout << "Correct" << endl;
+  }
+  else if (imageColor2 == "No"){
+    cout << "Incorrect. May need to turn on colorblind settings." << endl;
+  }
+  cout << "Do you see the color blue?(Yes/No)" << endl;
+  cin >> imageColor3;
+
+  if (imageColor3 == "Yes"){
+    cout << "Correct" << endl;
+  }
+  else if (imageColor3 == "No"){
+    cout << "Incorrect. May need to turn on colorblind settings." << endl;
+  }
   do{
-    cout << "Welcome, would you like to turn on colorblind settings? (Yes or No):";
+    cout << "Welcome, would you like to turn on colorblind settings for userimage.bmp? (Yes or No):";
     cin >> decision;
-  if (decision == "yes") 
+  if (decision == "Yes" || decision == "yes") 
   {
     cout << "Colorblind setttings enabled" << endl;
-    outfile << "Colorblind setttings: enabled" << endl;
   }
-  else if (decision == "no") 
+  else if (decision == "No" || decision == "no") 
   {
     cout << "Regular settings: enabled" << endl;
     
-    outfile << "Regular settings: enabled" << endl;
   }
   else {
     cout << "Please enter Yes or No." << endl;
   }
-  }while (decision != "yes" && decision != "no");
+  }while (decision != "Yes" && decision != "yes" &&       decision != "No" && decision != "no");
+
+    test.setColorBlindSetting(decision);
+    outfile << "Are color blind settings enabled: " << test.getColorBlindSetting()<< endl;
 
   outfile.close();
     cout << "Thank you for answering the questions, your results are in the results.txt file." << endl;
   
-  cout << "Does color blindness run in the family?(Y or N)" << endl;
-  cin >> answer;
+    cout << "Does color blindness run in the family?(Y or N)" << endl;
+    cin >> answer;
   if (answer == "Y") {
     string formatttedAnswer= "Yes color blindess runs in the family";
     answerVector.push_back(formatttedAnswer);
@@ -103,6 +114,5 @@ int main()
   for(int i=0; i < answerVector.size(); i++)
    cout << answerVector[i] << ' ';
   
-*/
   return 0;
 }
--- user.cpp
+++ user.cpp
@@ -1,8 +1,53 @@
-#include "student.h"
+#include "user.h"
 #include <string>
 #include <vector>
 
-using std::string;
-using std::vector;
+using namespace std;
 
 // Member function implementations...
+void user::setFirstName(string input){
+  firstName=input;
+}
+string user::getFirstName(){
+  return firstName;
+}
+void user::setLastName(string input){
+  lastName=input;
+}
+string user::getLastName(){
+  return lastName;
+}
+void user::setAnswer(string input){
+  answer=input;
+}
+string user::getAnswer(){
+  return answer;
+}
+void user::setDecision(string input){
+  decision=input;
+}
+string user::getDecision(){
+  return decision;
+}
+void user::setAge(int input){
+  age=input;
+}
+int user::getAge(){
+  return age;
+}
+void user::setGlasses(bool input){
+if( input== true){
+  glasses="yes";
+}
+  else
+  glasses="no";
+}
+string user::getGlasses(){
+  return glasses;
+}
+void user::setColorBlindSetting(string input){
+  colorBlindSetting=input;
+}
+string user::getColorBlindSetting(){
+  return colorBlindSetting;
+}
